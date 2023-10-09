6737ca4bade55348feb05c2011959290a8f5417d
martinsgit1211 <martinsishaya2000@gmail.com>
2023-10-09 12:06:50 +0100

    first commit

---
+++ action.js
@@ -0,0 +1,15 @@
+// const ITEMS_REQUEST = ‘ITEMS_REQUEST’ ;
+// const ITEMS_REQUEST_SUCCESS = ‘ITEMS_REQUEST_SUCCESS’ ;
+// export function itemsRequest(bool) {
+ 
+//    return {
+//       type: ITEMS_REQUEST,
+//       isLoading: bool,
+//    }
+// }
+// export function itemsRequestSuccess(bool) {
+//    return {
+//       type: ITEMS_REQUEST_SUCCESS,
+//       isLoading: bool,
+//    }
+// }
